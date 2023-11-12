cee7b34f5fac5848b7131d2acb8ca4d56c1dbe13
Bohdana Sherstyniuk <ms.sherstynyuk@gmail.com>
2023-11-12 14:10:30 +0200

    [BRING-13] added code styles config for IntelliJ IDEA

--- BringWeb/src/main/java/org/bobocode/hoverla/bring/web/Main.java
+++ BringWeb/src/main/java/org/bobocode/hoverla/bring/web/Main.java
@@ -1,7 +1,9 @@
 package org.bobocode.hoverla.bring.web;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+  }
+
 }
